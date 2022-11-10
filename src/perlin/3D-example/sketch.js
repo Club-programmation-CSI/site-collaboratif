a933bb80654c697dfbc5978c898288d803f804ef
rafalou38 <rafael.martinezcalvo@orange.fr>
2022-11-10 08:25:53 +0000

    Improve 3D example

--- src/perlin/3D-example/sketch.js
+++ src/perlin/3D-example/sketch.js
@@ -1,22 +1,23 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
-  camera(-200, -200, -200,   // camera position (x, y, z)
-    0, -100, 0,   // camera target (look at position) (x, y, z)
-    0, 1, 0);  // camera up axis: Y axis here
-  orbitControl();
-  //   normalMaterial();
+	createCanvas(400, 400, WEBGL);
+	camera(-200, -200, -200,   // camera position (x, y, z)
+		0, -100, 0,   // camera target (look at position) (x, y, z)
+		0, 1, 0);  // camera up axis: Y axis here
+	orbitControl();
+	//normalMaterial();
 }
 function draw() {
-  background(200);
-  orbitControl();
+	background(200);
+	orbitControl();
+	const sz = 20;
 
-  for (let x = 0; x < width; x += 20) {
-    for (let z = 0; z < height; z += 20) {
-      push();
-      // ground plane is XZ, not XY (front plane)
-      translate(x, 0, z);
-      box(20);
-      pop();
-    }
-  }
+	for (let x = 0; x < width; x += sz) {
+		for (let z = 0; z < height; z += sz) {
+			push();
+			// ground plane is XZ, not XY (front plane)
+			translate(x, sin(x / 20 + (millis() / 1000)) * (sz / 2), z);
+			box(sz);
+			pop();
+		}
+	}
 }
